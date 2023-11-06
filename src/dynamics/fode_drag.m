790cb3cfcfebb2f1470da5df3be5631beaf8fb24
ndelurgio <delurgion@gmail.com>
2023-11-06 13:40:07 -0800

    remove altitude warnings

--- src/dynamics/fode_drag.m
+++ src/dynamics/fode_drag.m
@@ -2,13 +2,11 @@ function xdot = fode_drag(t,x,mu,spacecraft,t_epoch,space_weather_data)
 date = t_epoch + seconds(t);
 LLA = eci2lla_datetime(x(1:3)',date);
 % Density Model
-% if norm(x(1:3)) > 1E6
-%     rho = 0;
-% else
-    [f107average,f107daily,magneticIndex] = fluxSolarAndGeomagnetic(date.Year,day(date,'dayofyear'),date.Second,space_weather_data);
-    [~, rho] = atmosnrlmsise00(LLA(3),LLA(1),LLA(2),date.Year,day(date,'dayofyear'),date.Second,f107average,f107daily,magneticIndex);
-    rho = rho(6); % This is the total parameter
-% end
+warning('off','aero:atmosnrlmsise00:invalidAltitude');
+[f107average,f107daily,magneticIndex] = fluxSolarAndGeomagnetic(date.Year,day(date,'dayofyear'),date.Second,space_weather_data);
+[~, rho] = atmosnrlmsise00(LLA(3),LLA(1),LLA(2),date.Year,day(date,'dayofyear'),date.Second,f107average,f107daily,magneticIndex);
+rho = rho(6); % This is the total parameter
+warning('on','aero:atmosnrlmsise00:invalidAltitude');
 % Drag Perturbation
 w_earth = [0;0;7.2921150E-5];
 v_rel = x(4:6) - cross(w_earth,x(1:3));
